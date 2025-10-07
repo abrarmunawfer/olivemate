397b8506d2ffd5b811fdb7f082c3137fab782784
abrarmunawfer <abrarmunawfer222@gmail.com>
2025-10-07 23:59:47 +0530

    dbupdate

--- connection/conn.php
+++ connection/conn.php
@@ -1,23 +1,6 @@
 <?php
 // Database credentials
 $servername = "localhost";          // Change to your hosting DB server
-$username   = "your_db_username";   // Your hosting DB username
-$password   = "your_db_password";   // Your hosting DB password
-$dbname     = "your_db_name";       // Your hosting database name
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected successfully";
-}
-?>
-<?php
-// Database credentials
-$servername = "localhost";          // Change to your hosting DB server
 $username   = "lexputjd_ngs";   // Your hosting DB username
 $password   = "olivengs2025";   // Your hosting DB password
 $dbname     = "lexputjd_olivemate";       // Your hosting database name
